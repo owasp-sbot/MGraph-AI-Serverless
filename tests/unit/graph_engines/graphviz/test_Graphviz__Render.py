65585b082c1c1e06287f44b7e782f6ae10028676
Dinis Cruz <dinis.cruz@owasp.org>
2025-01-25 16:32:00 +0000

    added "MGraph v1.0.0 CI/CD Pipeline Setup" and couple more dot tests

--- tests/unit/graph_engines/graphviz/test_Graphviz__Render.py
+++ tests/unit/graph_engines/graphviz/test_Graphviz__Render.py
@@ -1,11 +1,8 @@
-from unittest import TestCase
-
-from osbot_utils.utils.Files import file_exists
-
-from mgraph_ai.providers.simple.MGraph__Simple__Test_Data import MGraph__Simple__Test_Data
-from osbot_utils.utils.Dev import pprint
-
-from mgraph_ai_serverless.graph_engines.graphviz.Graphviz__Render import Graphviz__Render
+from unittest                                                       import TestCase
+from mgraph_ai.providers.json.MGraph__Json                          import MGraph__Json
+from osbot_utils.utils.Files                                        import file_exists, file_delete
+from mgraph_ai.providers.simple.MGraph__Simple__Test_Data           import MGraph__Simple__Test_Data
+from mgraph_ai_serverless.graph_engines.graphviz.Graphviz__Render   import Graphviz__Render
 
 
 class test_Graphviz__Render(TestCase):
@@ -20,3 +17,23 @@ class test_Graphviz__Render(TestCase):
         with self.graphviz_render as _:
             created_file = _.render_dot(self.dot_text)
             assert file_exists(created_file) is True
+            assert file_delete(created_file) is True
+
+    def test_render_dot__json_mgraph(self):
+        mgraph    = MGraph__Json()
+        test_data = { "string" : "value"         ,
+                      "number" : 42              ,
+                      "boolean": True            ,
+                      "null"   : None            ,
+                      "array"  : [1, 2, 3]       ,
+                      "object" : {"key": "value"}}
+
+        mgraph.load().from_json(test_data)
+        dot_text = mgraph.export().to_dot().to_string()
+        with self.graphviz_render as _:
+            created_file = _.render_dot(dot_text)
+            assert file_exists(created_file) is True
+            assert file_delete(created_file) is True
+
+
+
